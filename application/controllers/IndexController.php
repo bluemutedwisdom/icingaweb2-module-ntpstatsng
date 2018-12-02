85f6431753fc709d6bd24700c6dbb0b26e6ed7e2
WOLfgang Schricker <git@wols.org>
2018-12-02 23:06:25 +0100

    add menuentry and index

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+use Icinga\Web\Controller\ModuleActionController;
+
+class Ntpstatsng_IndexController extends ModuleActionController
+{
+    public function indexAction()
+    {
+
+    }
+}
---
+++ configuration.php
@@ -0,0 +1,3 @@
+<?php
+
+$section_1 = $this->menuSection(N_('NTPstats-NG'))->setUrl('ntpstatsng')->setIcon('clock')->setPriority(90);
