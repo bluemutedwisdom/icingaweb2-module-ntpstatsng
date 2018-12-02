2f10584b4521841e3510d6295b402715367a1eab
WOLfgang Schricker <git@wols.org>
2018-12-02 23:49:51 +0100

    add default tab

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,20 @@ class Ntpstatsng_IndexController extends ModuleActionController
 {
     public function indexAction()
     {
+        $this->getTabs()->activate('ntpstatsng');
+    }
 
+    public function getTabs()
+    {
+        $tabs = parent::getTabs();
+        $tabs->add(
+            'ntpstatsng',
+            array(
+                'title' => 'NTPstats-NG',
+                'url'   => 'ntpstatsng'
+            )
+        );
+
+        return $tabs;
     }
 }
